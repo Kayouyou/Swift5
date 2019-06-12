3b88d5d8bf626cd0276803f20ba0b8875f679e60
Mark <yeyangyang519716@gmail.com>
2019-06-12 15:03:14 +0800

    协议完结

--- Protocol.playground/Contents.swift
+++ Protocol.playground/Contents.swift
@@ -219,3 +219,225 @@ let myDice = [d6,d12]
 print(myDice.textualDescription)
 
 
+//在扩展里声明采纳协议
+//当一个协议已经符合了，某个协议中的所有要求，却还没有声明采纳该协议时，可以通过空的扩展来让它采纳该协议
+struct Hamster {
+    var name: String
+    var textualDescription: String {
+        return "A hamster named \(name)"
+    }
+}
+extension Hamster: TextRepresentable {}
+//从现在起，hamster的实例可以作为TextRepresentable类型使用
+
+let simonTheHamster = Hamster(name: "Simon")
+let somethingTextRepresentable: TextRepresentable = simonTheHamster
+print(somethingTextRepresentable.textualDescription)
+
+//即使满足了协议的所有的要求，类型也不会自动遵循协议，必须显示地遵循协议
+
+//协议类型可以在数组或字典这样的集合中使用，在协议类型提到了这样的用法
+let things: [TextRepresentable] = [game,d12,simonTheHamster]
+for thing in things {
+    print(thing.textualDescription)
+}
+
+
+//协议的继承
+protocol PrettyTextRepresentable: TextRepresentable {
+    var prettyTextualDescription: String { get }
+}
+
+//扩展SnakesAndLadders
+extension SnakesAndLadders: PrettyTextRepresentable {
+    var prettyTextualDescription: String {
+        var output = textualDescription + ":\n"
+        for index in 1...finalSquare {
+            switch board[index] {
+            case let ladder where ladder > 0:
+                output += "△"
+            case let snake where snake < 0:
+                output += "▽"
+            default:
+                output += "⭕️"
+            }
+        }
+        return output
+    }
+}
+
+print(game.prettyTextualDescription)
+
+//类专属的协议 通过添加anyobject关键字到协议的继承序列，可以限制协议只能被类类型采纳
+protocol SomeClassOnlyProtocol: AnyObject,SomeProtocol {
+    
+}
+//上面的这个协议只能被类类型采纳，如果是结构体或枚举类型采纳就会导致编译时错误
+
+//协议合成 要求一个类型同时遵循多个协议是很有用的
+protocol Named {
+    var name: String { get }
+}
+protocol Aged {
+    var age: Int { get }
+}
+struct People:Named,Aged {
+    var name: String
+    var age: Int
+}
+//意味着任何同时遵循named和aged的协议，不关心参数的具体类型，只要参数符合这两个协议即可
+func wishHappyBirthday(to celebrator: Named & Aged) {
+    print("Happy birthday,\(celebrator.name),you are \(celebrator.age)!")
+}
+let birthdayPerson = People(name: "Michael", age: 21)
+wishHappyBirthday(to: birthdayPerson)
+
+
+//类 和 协议进行组合
+//创建父类location
+class Location {
+    var latitude: Double
+    var longitude: Double
+    init(latitude: Double,longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
+
+//继承location 并遵循named协议
+class City: Location,Named {
+    var name: String
+    init(name: String, latitude: Double,longitude: Double) {
+        self.name = name
+        super.init(latitude: latitude, longitude: longitude)
+    }
+}
+
+//意味着任何location的子类，并且遵循named协议参数都满足
+func beginConcert(in location: Location & Named) {
+    print("Hello,\(location.name)!")
+}
+
+let seattle = City(name: "Seattle", latitude: 47.5, longitude: -122.3)
+beginConcert(in: seattle)
+
+
+//协议的一致性
+protocol HashArea {
+    var area: Double { get }
+}
+class Circle: HashArea {
+    let pi = 3.1415926
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) {
+        self.radius = radius
+    }
+}
+class Country: HashArea {
+    var area: Double
+    init(area: Double) {
+        self.area = area
+    }
+}
+class Animal {
+    var legs: Int
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
+
+// circle country animal并没有一个共同的基类，尽管如此，它们都是类，它们的实例都可以作为anyobject类型的值
+let objects:[AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
+
+//objects数组使用字面量初始化
+for object in objects {
+    if let objectWithArea = object as? HashArea {
+        print("Area is \(objectWithArea)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
+
+//可选的协议要求
+//@objc标记如此的协议只能被继承自objective-c类的类或者@objc类遵循，其他类以及结构体和枚举均不能遵循这种协议
+@objc protocol CounterDataSource {
+    //使用可选要求时，它们的类型会自动变成可选的
+    @objc optional func increment(forCount count: Int) -> Int
+    @objc optional var fixedIncrement: Int { get }
+}
+
+class Counter {
+    var count = 0
+    var dataSources: CounterDataSource?
+    func increment() {
+        if let amount = dataSources?.increment?(forCount: count) {
+            count += amount
+        } else if let amount = dataSources?.fixedIncrement {
+            count += amount
+        }
+    }
+}
+
+class ThreeSource: NSObject,CounterDataSource {
+    let fixedIncrement: Int = 3
+}
+
+var counter = Counter()
+counter.dataSources = ThreeSource()
+for _ in 1...4{
+    counter.increment()
+    print(counter.count)
+}
+
+class TowardsZeroSource: NSObject,CounterDataSource {
+    func increment(forCount count: Int) -> Int {
+        if count == 0 {
+            return 0
+        } else if count < 0 {
+            return 1
+        } else {
+            return -1
+        }
+    }
+}
+
+counter.count = -4
+counter.dataSources = TowardsZeroSource()
+for _ in 1...5 {
+    counter.increment()
+    print(counter.count)
+}
+
+//为协议扩展添加限制条件
+extension Collection where Element: Equatable{
+    func allEqual() -> Bool {
+        for element in self {
+            if element != self.first {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+let equalNumbers = [100,100,100,100,100]
+let differentNumbers = [100,100,200,100,200]
+
+print(equalNumbers.allEqual())
+print(differentNumbers.allEqual())
+
+
+
+
+
+
+
+
+
+
+
